fc6d88837e868a925195faf78dc8cdc95607a819
AkhnazarovaA <akhnazarova.ainura@gmail.com>
2026-05-15 14:31:22 +0500

    add one-style record dto-s

--- src/main/java/com/ainura/finance_tracker/item/model/dto/ItemResponse.java
+++ src/main/java/com/ainura/finance_tracker/item/model/dto/ItemResponse.java
@@ -2,53 +2,9 @@ package com.ainura.finance_tracker.item.model.dto;
 
 import java.math.BigDecimal;
 
-public class ItemResponse {
-
-    private Long id;
-    private String name;
-    private BigDecimal amount;
-    private Integer quantity;
-    private Long transactionId;
-
-    public ItemResponse(Long id, String name, BigDecimal amount, Integer quantity, Long transactionId) {
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
-        this.quantity = quantity;
-        this.transactionId = transactionId;
-    }
-    public ItemResponse() {}
-
-    public Long getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public BigDecimal getAmount() {
-        return amount;
-    }
-    public Integer getQuantity() {
-        return quantity;
-    }
-    public Long getTransactionId() {
-        return transactionId;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-    public void setTransactionId(Long transactionId) {
-        this.transactionId = transactionId;
-    }
-
+public record ItemResponse(Long id,
+                           String name,
+                           BigDecimal amount,
+                           Integer quantity,
+                           Long transactionId) {
 }
--- src/main/java/com/ainura/finance_tracker/transaction/model/dto/income/TotalIncomeResponse.java
+++ src/main/java/com/ainura/finance_tracker/transaction/model/dto/income/TotalIncomeResponse.java
@@ -2,14 +2,5 @@ package com.ainura.finance_tracker.transaction.model.dto.income;
 
 import java.math.BigDecimal;
 
-public class TotalIncomeResponse {
-
-    private BigDecimal totalIncome;
-
-    public TotalIncomeResponse(BigDecimal totalIncome) {
-        this.totalIncome = totalIncome;
-    }
-    public BigDecimal getTotalIncome() {
-        return totalIncome;
-    }
+public record TotalIncomeResponse(BigDecimal totalIncome) {
 }
